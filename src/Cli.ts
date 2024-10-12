f675230ef0af1b29f87260f704bc487a0d51300b
joshcord99 <joshcord99@gmail.com>
2024-10-11 17:16:31 -0700

    added to choices array

--- src/Cli.ts
+++ src/Cli.ts
@@ -15,25 +15,26 @@ const pool = new Pool({
 
 pool.connect((err: any) => {
     if (err) {
-        console.error('Database connection error:', err.stack);
+        console.error('Database connection error:');
     } else { console.log('Database connected'); }
 });
 
-function main() {
+function mainMenu() {
     inquirer
         .prompt([
             {
                 type: 'list',
                 name: 'userChoices',
-                message: 'This is a list of all the employees',
+                message: 'Here you go !',
                 choices: [
                     'View All Employees',
                     'View All Departments',
-                    'View All Roles',
+                    'View All Role',
                     'Add Employee',
                     'Add Role',
                     'Update Employee Role',
-                    'Update Role'],
+                    'Update Role',
+                    'Exit'],
             },
         ])
         .then(res => {
@@ -41,160 +42,166 @@ function main() {
                 case 'View All Employees':
                     viewAllEmployees();
                     break;
-                    case 'View All Departments':
-                        viewAllDepartments();
-                        break;
-                    case 'Add Department':
-                        addDepartment();
-                        break;
-                    case 'Add Employee':
-                        addEmployee();
-                        break;
-                    case 'Add Role':
-                        addRole();
-                        break;
-                    case 'Update Employee Role':
-                        updateRole();
-                        break;
-                    case 'View All Role':
-                        viewAllRole();
-                        break;
+                case 'View All Departments':
+                    viewAllDepartments();
+                case 'Add Department':
+                    addDepartment();
+                    break;
+                case 'Add Employee':
+                    addEmployee();
+                    break;
+                case 'Add Role':
+                    addRole();
+                    break;
+                case 'Update Employee Role':
+                    updateRole();
+                    break;
+                case 'View All Role':
+                    viewAllRole();
+                    break;
+                    default:
+                        pool.end();
+                        process.exit();
             }
         })
 }
-
-
-// method to start the cli
-function addEmployee() {
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'name',
-                message: 'What is the employees name? ',
-            },
-            {
-                type: 'input',
-                name: 'role',
-                message: 'What is his/her role at the company?',
-            },
-            {
-                type: 'input',
-                name: 'manager',
-                message: 'What is his/her assigned manager name?',
-            },
-        ])
-}
-function viewAllEmployees() {
-    const query = "SELECT employee.id, employee.first_name, employee.last_name, role.title, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee JOIN role ON employee.role_id = role.id LEFT JOIN employee AS manager ON employee.manager_id = manager.id";
-    pool.query(query, (err, res) => {
-        if (err) throw err;
-        console.table(res.rows);
-        main();
+const viewAllDepartments = () => {
+    const query = "SELECT * FROM department;";
+    pool.query(query, (err, result) => {
+        if (err) {
+            console.error('Uh Oh! Error fetching departments');
+        } else {
+            console.table(result.rows);
+        }
+        mainMenu();
     });
-}
-function addDepartment() {
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'name',
-                message: 'What is the department name? ',
-            },
-        ])
-}
+};
 
-function viewAllDepartments() {
-    inquirer
-        .prompt([
-            {
-                type: 'list',
-                name: 'departmentList',
-                message: 'This is a list of all the Departments in your company',
-                choices: [''],
-            },
-        ])
-}
+const viewAllRole = () => {
+    const query = `
+        SELECT department.name AS department_name, 
+               role.salary AS role_salary, 
+               role.title AS role_title 
+        FROM role 
+        JOIN department 
+        ON role.department_id = department.id;`;
+    pool.query(query, (err, result) => {
+        if (err) {
+            console.error('Uh Oh! Error fetching roles');
+        } else {
+            console.table(result.rows);
+        }
+        mainMenu();
+    });
+};
 
-function addRole() {
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'roleName',
-                message: 'What is the name of the role?',
-            },
-            {
-                type: 'input',
-                name: 'salaryRole',
-                message: 'What is the salary for this particualar role?',
-            },
-            {
-                type: 'list',
-                name: 'departmentRole',
-                message: 'Which department belongs to this role?',
-                choices: [''],
-            },
-        ])
-}
-function updateRole() {
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'employeeName',
-                message: 'What is the employee\'s name? ',
-            },
-            {
-                type: 'input',
-                name: 'newRole',
-                message: 'What do you want to update this employee\'s role to?',
-            },
-        ]);
-}
-function viewAllRole() {
-    inquirer
-        .prompt([
-            {
-                type: 'list',
-                name: 'roleList',
-                message: 'This is a list of all the roles in your company',
-                choices: [''],
-            },
-        ])
-}
-function startCli() {
-    inquirer
-        .prompt([
-            {
-                type: 'list',
-                name: 'CreateOrSelect',
-                message: 'What would you like to do?',
-                choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department'],
-            },
-        ])
-        .then((answers) => {
-            if (answers.CreateOrSelect === 'View All Employees') {
-                viewAllEmployees();
+
+const viewAllEmployees = () => {
+    const query = "SELECT employee.first_name, employee.last_name, role.title, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee JOIN role ON employee.role_id = role.id LEFT JOIN employee AS manager ON employee.manager_id = manager.id;";
+    pool.query(query, (err, result) => {
+        if (err) {
+            console.error('Uh Oh! Error fetching employees');
+        } else {
+            console.table(result.rows);
+        }
+        mainMenu();
+    });
+};
+const addDepartment = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter the name of the new department:',
+        }
+    ]).then(answer => {
+        const query = 'INSERT INTO department (name) VALUES ($1);';
+        pool.query(query, [answer.name], (err) => 
+            if (err) {
+                console.error('Uh Oh! Error adding department');
+        });
+    });
+};
+
+const addRole = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is name of the new role?:',
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'What is the salary for this particular role?:',
+        },
+        {
+            type: 'input',
+            name: 'department_id',
+            message: 'What is the department ID that this new role falls under?:',
+        }
+    ]).then(answer => {
+        const query = 'INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3);';
+        pool.query(query, [answer.title, answer.salary, answer.department_id], (err) => {
+            if (err) {
+                console.error('Uh Oh! Error adding role');
+
+        });
+    });
+};
+
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: 'What is the new employee\'s first name?:',
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'What is the new employee\'s last name?',
+        },
+        {
+            type: 'input',
+            name: 'role_id',
+            message: 'What is the employee role ID?:',
+        },
+        {
+            type: 'input',
+            name: 'manager_id',
+            message: "What is the new employee's boss's ID number?:",
+        }
+    ]).then(answer => {
+        const query = 'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4);';
+        pool.query(query, [answer.first_name, answer.last_name, answer.role_id, answer.manager_id], (err) => {
+            if (err) {
+                console.error('Uh Oh! Error adding new employee');
             }
-            else if (answers.CreateOrSelect === 'View All Roles') {
-                viewAllRole();
-            }
-            else if (answers.CreateOrSelect === 'View All Departments') {
-                viewAllDepartments();
-            }
-            else if (answers.CreateOrSelect === 'Add Employee') {
-                addEmployee();
-            }
-            else if (answers.CreateOrSelect === 'Update Employee Role') {
-                updateRole();
-            }
-            else if (answers.CreateOrSelect === 'Add Role') {
-                addRole();
-            } else if (answers.CreateOrSelect === 'Add Department') {
-                addDepartment();
+            mainMenu();
+        });
+    });
+};
+const updateRole = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'employee_id',
+            message: 'Please enter the employee ID:',
+        },
+        {
+            type: 'input',
+            name: 'role_id',
+            message: 'Please enter the new role ID:',
+        }
+    ]).then(answer => {
+        const query = 'UPDATE employee SET role_id = $1 WHERE id = $2;';
+        pool.query(query, [answer.role_id, answer.employee_id], (err) => {
+            if (err) {
+                console.error('Uh Oh! Error updating employee\'s role');
             }
         });
-}
+    });
+};
 
-main();
+mainMenu();
